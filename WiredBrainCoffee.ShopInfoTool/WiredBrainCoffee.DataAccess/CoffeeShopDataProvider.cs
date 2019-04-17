929fbacff81ce88ea97cc98528b39bb7cf3ab597
Johan Lång <johan.lang@hotmail.com>
2019-04-17 13:40:14 +0200

    Add data access project to solution

---
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.DataAccess
+{
+	public class CoffeeShopDataProvider
+	{
+		public IEnumerable<CoffeeShop> LoadCoffeShops()
+		{
+			yield return new CoffeeShop { Location = "Gothenburg", BeanInStockInKg = 106 };
+			yield return new CoffeeShop { Location = "Orebro", BeanInStockInKg = 66 };
+			yield return new CoffeeShop { Location = "Stockholm", BeanInStockInKg = 26 };
+		}
+	}
+}
--- WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrainCoffee.ShopInfoTool
 {
@@ -7,6 +8,29 @@ namespace WiredBrainCoffee.ShopInfoTool
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Wirded Brain Coffe - Shop Info Tool!");
+
+			Console.WriteLine("Write help to list available commands");
+
+			var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+			while (true)
+			{
+				var line = Console.ReadLine();
+
+				var coffeeShops = coffeeShopDataProvider.LoadCoffeShops();
+
+				if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+				{
+					Console.WriteLine("> Available coffee shops commands:");
+
+					foreach (var coffeshop in coffeeShops)
+					{
+						Console.WriteLine($"> {coffeshop.Location}");
+					}
+				}
+
+
+			}
 		}
 	}
 }
